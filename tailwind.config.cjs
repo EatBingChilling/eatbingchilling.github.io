29cb9ea1f70ffb4e306da1565face9b982d06989
EatBingChilling <2284257190@qq.com>
2025-11-01 09:27:43 +0800

    a

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,26 @@
+// tailwind.config.cjs
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ['Roboto', 'sans-serif', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
-}
+  plugins: [
+    require('@tailwindcss/typography'),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.link': {
+          '@apply text-[var(--primary)] underline decoration-[var(--link-underline)] decoration-1 decoration-dashed underline-offset-4': {},
+        },
+      });
+    }),
+  ],
+};
